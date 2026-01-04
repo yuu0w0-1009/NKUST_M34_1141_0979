b4e333a314f5f4e4c1438a34b5992ad8a6bc9e7c
yuu0w0-1009 <5322brian@gmail.com>
2026-01-04 21:03:14 +0800

    FE_25_12_29_2

--- final exam/25_12_29/04.py
+++ final exam/25_12_29/04.py
@@ -1,12 +1,12 @@
 import time
-from random import randint as rdi
+from random import randint
 
 x = 1
 ans = [0]*4
 
 while(1):
     print(f'第 {x} 次 :')
-    t = rdi(1,5)
+    t = randint(1,5)
     time.sleep(t)
     print("時間到!",end="")
     t1 = time.time()
